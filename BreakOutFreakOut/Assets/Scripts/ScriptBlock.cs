af2cca3e2f58993cc30d5486b6309005eb8415e5
julmasmurffi <js.kaikkonen@gmail.com>
2019-03-16 13:23:58 +0200

    Implemented many levels of damage and new system to display sprites of various damage levels

--- BreakOutFreakOut/Assets/Scripts/ScriptBlock.cs
+++ BreakOutFreakOut/Assets/Scripts/ScriptBlock.cs
@@ -1,12 +1,22 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ScriptBlock : MonoBehaviour {
+
+    //config params
     [SerializeField] AudioClip breakBlock;
     [SerializeField] GameObject blockSparks;
+    [SerializeField] int maxHits;
+    [SerializeField] Sprite[] hitSprites;
 
+    //cached ref
     level level;
+
+
+    //state vars
+    [SerializeField] int timesHit; //TODO only serialized for debugging
 
     private void Start()
     {
@@ -22,19 +32,34 @@ public class ScriptBlock : MonoBehaviour {
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (tag == "Breakable") { DestroyBlock(); }  
+        if (tag == "Breakable")
+        {
+            HandleHit();
+        }
+        else
+        {
+            ShowNextHitSprite();
+        }
+    }
+
+    private void ShowNextHitSprite()
+    {
+        int spriteIndex = timesHit - 1;
+        GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+    }
+
+    private void HandleHit()
+    {
+        timesHit++;
+        if (timesHit >= maxHits) { DestroyBlock(); }
     }
 
     private void DestroyBlock()
     {
-        
-        
             TriggerSparklesVFX();
             PlayBlockDestroy();
             Destroy(gameObject);
-
             level.BlockDestroyed();
-        
     }
 
     private void PlayBlockDestroy()
